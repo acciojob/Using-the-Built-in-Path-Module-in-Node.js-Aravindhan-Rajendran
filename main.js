4cdc02f66ba26809997f323d3e8efb5ecde2135d
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-18 16:18:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const path = require('path');
 
-// TODO: Use path.join to join two file path segments
+// Get the file path segments from command-line arguments
+const segment1 = process.argv[2];
+const segment2 = process.argv[3];
 
-// TODO: Print the resulting path to the console
+// Function to join the file paths
+function joinPaths(segment1, segment2) {
+  return path.join(segment1, segment2);
+}
+
+// Join the paths and print the result to the console
+const joinedPath = joinPaths(segment1, segment2);
+console.log(joinedPath);
+
+// DO NOT EDIT BELOW THIS LINE
+module.exports = { joinPaths };
